de1ac1a08558770faafe96dc6c36d1fd221b11d6
Stanciu Andrei <51262522+puya2003205@users.noreply.github.com>
2020-03-07 15:24:40 +0200

    Add files via upload

--- exercitiu.py
+++ exercitiu.py
@@ -16,6 +16,37 @@ auth.set_access_token(access_token_key, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 api.verify_credentials()
 
-usr_id=api.get_user(screen_name="Puya999").id
-directMessage=api.send_direct_message(usr_id, "Acesta este un mesaj")
+usr_id=api.get_user(screen_name="saradagurl").id
+directMessage=api.send_direct_message(usr_id, "Bau")
 print("Mesajul a fost trimis")
+
+#try:
+ #   api.update_status("bau cica bau bau") ##endpoint statuses/update
+#except:
+ #   print("Nu se posta un nou tweeet")
+
+#try:
+ #   api.destroy_status(1236272019819372544)
+  #  print("Tweet-ul a fost sters")
+#except:
+ #   None
+
+#try:
+ #   user=api.get_user(screen_name="Puya999") ##inlocuieste parametrul cu user-ul de twitter dorit  #endpoint users/show.json
+  #  print("ID-ul intern al user-ului %s" %user.id)
+   # print("Followers %s" % user.followers_count)
+    #print("Lista prieteni: ")
+    #for friend in user.friends():
+     #   print friend.screen_name
+#except:
+ #   print("User-ul specificat nu poate fi gasit")
+
+# print("Lista tweet-uri care contin cuvantul Python")
+# search_results=api.search(q="python", count=10) ##q este cuvantul cautat in tweets,iar count reprezinta numerele de rezulate dorite
+# for i in search_results:#iterator printre inregistrarile returnate de interogare
+    # print("Text %s" %i.text)
+    # print("Location %s" % i.user.location)
+    # print("Name %s" % i.user.name)
+    # print("Data %s" % i.created_at)
+    # print("--------------------------------------------------------------")
+# 
